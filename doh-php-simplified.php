21e25e3f72d2dfa3d705e55070a1d94809191d4c
perezbox <39946168+perezbox@users.noreply.github.com>
2024-12-17 15:57:31 -0600

    Update doh-php-simplified.php

--- doh-php-simplified.php
+++ doh-php-simplified.php
@@ -1,106 +1,18 @@
 <?php
 
-/* PHP client implementation of DoH (DNS over HTTPS).
- * Supports A, AAAA, CNAME, MX, and NS records.
- */
-
-/* Domain str to DNS raw qname */
-function doh_domain2raw($domainname)
-{
-    $raw = "";
-    $domainpieces = explode('.', $domainname);
-    foreach ($domainpieces as $domainbit) {
-        $raw .= chr(strlen($domainbit)) . $domainbit;
-    }
-    $raw .= chr(0);
-    return $raw;
-}
-
-/* DNS raw qname to domain str */
-function doh_raw2domain($response, &$offset)
-{
-    $domainname = "";
-    $jumped = false;
-    $original_offset = $offset;
-
-    while (true) {
-        if ($offset >= strlen($response)) {
-            die("Error: Offset out of bounds while parsing domain name.\n");
-        }
-
-        $len = ord($response[$offset]);
-
-        if ($len === 0) {
-            $offset++;
-            break;
-        }
-
-        if (($len & 0xC0) === 0xC0) {
-            if (!$jumped) {
-                $original_offset = $offset + 2;
-            }
-            $pointer_offset = (($len & 0x3F) << 8) | ord($response[$offset + 1]);
-            $offset = $pointer_offset;
-            $jumped = true;
-            continue;
-        }
-
-        $offset++;
-        if ($offset + $len > strlen($response)) {
-            die("Error: Length out of bounds while parsing domain name.\n");
-        }
-        $domainname .= substr($response, $offset, $len) . ".";
-        $offset += $len;
-    }
-
-    if (!$jumped) {
-        $offset = $original_offset;
-    }
-
-    return rtrim($domainname, ".");
-}
-
-/* DNS type names to raw types */
-function doh_get_qtypes($requesttype = "A")
-{
-    $types = [
-        "A" => 1,
-        "AAAA" => 28,
-        "CNAME" => 5,
-        "MX" => 15,
-        "NS" => 2
-    ];
-    return $types[$requesttype] ?? 1;
-}
-
-/* Generate a DNS raw query */
-function doh_generate_dnsquery($domainname, $requesttype = "A")
-{
-    $rawtype = doh_get_qtypes($requesttype);
-    $dns_query = sprintf("\xab\xcd") . chr(1) . chr(0) .
-        chr(0) . chr(1) .  /* qdc */
-        chr(0) . chr(0) .  /* anc */
-        chr(0) . chr(0) .  /* nsc */
-        chr(0) . chr(0) .  /* arc */
-        doh_domain2raw($domainname) .
-        chr(0) . chr($rawtype) .
-        chr(0) . chr(1);  /* qclass */
-    return $dns_query;
-}
+/* ... (all functions as previously defined) ... */
 
 /* Connects via HTTPS to remote DoH servers */
 function doh_connect_https($dnsquery)
 {
     global $argv;
 
-    // Base64URL encode the DNS query
     $encoded_dnsquery = str_replace(
         ['+', '/', '='],
         ['-', '_', ''],
         base64_encode($dnsquery)
     );
 
-    // Determine the DoH server URL
     $doh_url = match ($argv[1]) {
         "cloudflare" => "https://cloudflare-dns.com/dns-query?dns=$encoded_dnsquery",
         "google" => "https://dns.google/dns-query?dns=$encoded_dnsquery",
@@ -108,7 +20,6 @@ function doh_connect_https($dnsquery)
         default => die("Error: Unsupported DoH server.\n"),
     };
 
-    // Set up cURL request
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $doh_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -134,22 +45,24 @@ function doh_read_dnsanswer($response, $requesttype)
     $results = [];
     $offset = 12;
 
+    // Debug raw response
+    echo "Raw response (hex): " . bin2hex($response) . "\n";
+
     $header = unpack("nTransactionID/nFlags/nQDCount/nANCount/nNSCount/nARCount", substr($response, 0, 12));
+    print_r($header);
 
     if (empty($header['nANCount'])) {
         echo "No answers found in the response.\n";
         return $results;
     }
 
-    // Skip questions
     while ($header['nQDCount']-- > 0) {
         while (ord($response[$offset]) > 0) {
             $offset += ord($response[$offset]) + 1;
         }
-        $offset += 5; // Null byte + QTYPE + QCLASS
+        $offset += 5;
     }
 
-    // Parse answer records
     while ($header['nANCount']-- > 0) {
         $name = doh_raw2domain($response, $offset);
 
@@ -167,6 +80,8 @@ function doh_read_dnsanswer($response, $requesttype)
                 $results[] = "Priority $priority - $host";
             } elseif ($requesttype === "NS") {
                 $results[] = doh_raw2domain($response, $offset - $record['nLength']);
+            } elseif ($requesttype === "A" || $requesttype === "AAAA") {
+                $results[] = inet_ntop($data);
             }
         }
     }
